7720787e8cc79032eb3971dfa90037475b6c0d3c
cell <chengbo.cell@gmail.com>
2025-08-16 00:28:25 +0800

    Initial Commit

---
+++ jlpt/jlptApp.swift
@@ -0,0 +1,17 @@
+//
+//  jlptApp.swift
+//  jlpt
+//
+//  Created by Bo Cheng on 2025/8/16.
+//
+
+import SwiftUI
+
+@main
+struct jlptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jlptTests/jlptTests.swift
@@ -0,0 +1,17 @@
+//
+//  jlptTests.swift
+//  jlptTests
+//
+//  Created by Bo Cheng on 2025/8/16.
+//
+
+import Testing
+@testable import jlpt
+
+struct jlptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
